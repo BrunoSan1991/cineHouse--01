f3b557b63d7b476a68c552b24d1d0734416d3e5d
Bruno Sanches <bsan.designgrafico@gmail.com>
2022-04-26 21:38:33 -0300

    alteração na função

--- cinema.js
+++ cinema.js
@@ -10,4 +10,79 @@ function listarTodosOsFilmes(filmes) {
     console.log(filmes[i].titulo);
   }
 }
+
+
 listarTodosOsFilmes(catalogoObj.data);
+
+function listarFilmesEmCartaz(filmes) {
+  const filmesEmCartaz = filmes.filter((filme) => {
+    if (filme.emCartaz === true) {
+      return true;
+    }
+  });
+  return filmesEmCartaz;
+}
+
+
+console.log(listarFilmesEmCartaz(catalogoObj.data));
+
+function alterarStatusEmCartaz(id, filmes) {
+  const idxFilme = filmes.findIndex((filme) => {
+    if (filme.codigo === id) {
+      return true;
+    }
+  });
+  if (idxFilme >= 0) {
+    filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(alterarStatusEmCartaz(1, catalogoObj.data));
+console.log(catalogoObj.data);
+var catalogo = require("./database/catalogo.json");
+// console.log(catalogo);
+var catalogoString = JSON.stringify(catalogo);
+// console.log(catalogoString);
+var catalogoObj = JSON.parse(catalogoString);
+// console.log(catalogoObj);
+
+function listarTodosOsFilmes(filmes) {
+  for (let i = 0; i < filmes.length; i++) {
+    console.log(filmes[i].titulo);
+  }
+}
+
+
+listarTodosOsFilmes(catalogoObj.data);
+
+function listarFilmesEmCartaz(filmes) {
+  const filmesEmCartaz = filmes.filter((filme) => {
+    if (filme.emCartaz === true) {
+      return true;
+    }
+  });
+  return filmesEmCartaz;
+}
+
+
+console.log(listarFilmesEmCartaz(catalogoObj.data));
+
+function alterarStatusEmCartaz(id, filmes) {
+  const idxFilme = filmes.findIndex((filme) => {
+    if (filme.codigo === id) {
+      return true;
+    }
+  });
+  if (idxFilme >= 0) {
+    filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(alterarStatusEmCartaz(1, catalogoObj.data));
+console.log(catalogoObj.data);
